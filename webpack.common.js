49a622c97e4709d374da62bcabed899f3d7d7114
maulanafikriash <maulanx835@gmail.com>
2023-10-06 12:46:19 +0700

    change config

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/MyExperience-With-DevHandal/',
   },
   module: {
     rules: [
